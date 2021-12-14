f3f8da1b2e7e57df87fcf596a650342c3166b9cd
petesalcedo <peter.salcedo.diaz@gmail.com>
2021-12-13 22:17:24 -0500

    adding css swiper cdn

--- Public/JS/script.js
+++ Public/JS/script.js
@@ -1,14 +1,13 @@
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("slide");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1} 
-  x[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel, 2000); 
-}
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 180,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop:true,
+});
